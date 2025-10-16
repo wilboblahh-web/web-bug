17b4128e2f3f525bc86eb47fa2ce628ec9999b93
wilboblahh-web <wilboblahh@gmail.com>
2025-10-16 22:28:03 +0700

    config.js

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "7467740518:AAFgqjLDnvMUZLBu1WfgLFDOrMiO2z2TpqQ",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7305534611", // Ubah Jadi Id Mu !!!
+  port: "2002", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "152.42.226.186" // Ubah Jadi Ip Vps Mu !!!
+};
